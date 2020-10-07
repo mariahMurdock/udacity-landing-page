67d0969c2b864b4f7f8eb6d11692074ac8a21a6e
mariahMurdock <mariahmurdock1227@gmail.com>
2020-10-06 23:07:31 -0700

    add button with class attribute to each navbar section. add anchor tag inside of button

--- js/script.js
+++ js/script.js
@@ -1,12 +1,30 @@
+  // const hrefList = ['#section1', '#section2', '#section3', '#section4'];
+  // const textContentList = ['Section One', 'Section Two', 'Section Three', 'Section Four'];
+  //
+  // for (let i=0; i < hrefList.length; i++) {
+  //   const navBarUl = document.querySelector('#navbar__list');
+  //   const navLi = document.createElement('li');
+  //   navBarUl.appendChild(navLi);
+  //   const aTag = document.createElement('a');
+  //   aTag.setAttribute('href', hrefList[i]);
+  //   aTag.textContent = textContentList[i];
+  //   navLi.appendChild(aTag);
+  // }
+
+
   const hrefList = ['#section1', '#section2', '#section3', '#section4'];
   const textContentList = ['Section One', 'Section Two', 'Section Three', 'Section Four'];
+  const classList = ['.class1', '.class2', '.class3', '.class4'];
 
   for (let i=0; i < hrefList.length; i++) {
     const navBarUl = document.querySelector('#navbar__list');
     const navLi = document.createElement('li');
     navBarUl.appendChild(navLi);
+    const button = document.createElement('button');
+    button.setAttribute('class', classList[i]);
     const aTag = document.createElement('a');
     aTag.setAttribute('href', hrefList[i]);
     aTag.textContent = textContentList[i];
-    navLi.appendChild(aTag);
+    button.appendChild(aTag);
+    navLi.appendChild(button);
   }
